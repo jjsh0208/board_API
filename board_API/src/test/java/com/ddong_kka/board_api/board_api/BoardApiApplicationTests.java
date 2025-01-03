2f1484f01196f72263693c1d0923689ce59afe59
jjsh0208 <jjsh0208@gmail.com>
2025-01-03 11:08:50 +0900

    Initial commit

---
+++ board_API/src/test/java/com/ddong_kka/board_api/board_api/BoardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ddong_kka.board_api.board_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
