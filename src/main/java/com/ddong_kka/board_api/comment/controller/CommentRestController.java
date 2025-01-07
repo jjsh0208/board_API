fc15cab6ef8d2d4ecf582d38af9d433f3b5d06d8
jjsh0208 <jjsh0208@gmail.com>
2025-01-07 16:12:27 +0900

    feat : Comment Update 기능 구현 - CommentRestController에서 /api/v1/comment/{id} 경로의 PUT 요청을 처리하여 댓글 수정 기능 구현 - ID는 댓글의 ID를 사용 - 예외처리를 위한 CommentNotFoundException 클래스 구현

--- src/main/java/com/ddong_kka/board_api/comment/controller/CommentRestController.java
+++ src/main/java/com/ddong_kka/board_api/comment/controller/CommentRestController.java
@@ -3,6 +3,10 @@ package com.ddong_kka.board_api.comment.controller;
 import com.ddong_kka.board_api.board.controller.BoardRestController;
 import com.ddong_kka.board_api.comment.dto.CommentWriteDto;
 import com.ddong_kka.board_api.comment.service.CommentService;
+import com.ddong_kka.board_api.exception.BoardNotFoundException;
+import com.ddong_kka.board_api.exception.CommentNotFoundException;
+import com.ddong_kka.board_api.exception.UnauthorizedAccessException;
+import com.ddong_kka.board_api.exception.UserNotFoundException;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.slf4j.Logger;
@@ -82,6 +86,89 @@ public class CommentRestController {
         }
     }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<?> update(@Valid @RequestBody CommentWriteDto commentWriteDto
+            , BindingResult bindingResult
+            , @PathVariable("id") Long id
+            , @RequestHeader("Authorization") String authorizationHeader
+    ){
+        String jwtToken = null;
+        try {
+            if (authorizationHeader != null && !authorizationHeader.startsWith("Bearer ")) {
+                throw new IllegalArgumentException("JWT token is missing or invalid");
+            }
 
+            jwtToken = authorizationHeader.substring(7);  // 'Bearer ' 부분을 제거하고 JWT만 추출
+
+            if (bindingResult.hasErrors()) {
+                List<Map<String, String>> errors = bindingResult.getFieldErrors().stream()
+                        .map(fieldError -> {
+                            logger.error("{} : {}", fieldError.getField(), fieldError.getDefaultMessage());
+                            return Map.of(
+                                    "field", fieldError.getField(),
+                                    "message", fieldError.getDefaultMessage()
+                            );
+                        }).toList();
+                Map<String, Object> response = Map.of(
+                        "status", HttpStatus.BAD_REQUEST.value(),
+                        "error", "Validation Field",
+                        "message", errors
+                );
+
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+            }
+
+            commentService.updateComment(commentWriteDto,id,jwtToken);
+
+            Map<String,Object> successResponse = Map.of(
+                    "message", "댓글 수정 성공"
+            );
+
+            return ResponseEntity.ok(successResponse);
+
+
+        }  catch(IllegalArgumentException e){
+            logger.warn("JWT Token NotFond - {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(Map.of(
+                            "status", HttpStatus.UNAUTHORIZED.value(),
+                            "error", "Missing JWT Token",
+                            "message", e.getMessage()
+                    ));
+        }catch (UserNotFoundException e) {
+            logger.warn("User NotFound - {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of(
+                            "status", HttpStatus.NOT_FOUND.value(),
+                            "error", "User Not Found",
+                            "message", e.getMessage()
+                    ));
+        } catch (CommentNotFoundException e) {
+            logger.warn("Comment NotFound - {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                    .body(Map.of(
+                            "status", HttpStatus.NOT_FOUND.value(),
+                            "error", "Comment Not Found",
+                            "message", e.getMessage()
+                    ));
+        } catch (UnauthorizedAccessException e) {
+            logger.warn("Unauthorized Access - {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN)
+                    .body(Map.of(
+                            "status", HttpStatus.FORBIDDEN.value(),
+                            "error", "Unauthorized Access",
+                            "message", e.getMessage()
+                    ));
+        }
+        catch (Exception e) {
+            logger.error("Unexpected Error - {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of(
+                            "status", HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                            "error", "Unexpected Error",
+                            "message", "예상치 못한 오류가 발생하였습니다."
+                    ));
+        }
+    }
 
 }
--- src/main/java/com/ddong_kka/board_api/comment/service/CommentService.java
+++ src/main/java/com/ddong_kka/board_api/comment/service/CommentService.java
@@ -7,6 +7,8 @@ import com.ddong_kka.board_api.comment.domain.Comment;
 import com.ddong_kka.board_api.comment.dto.CommentWriteDto;
 import com.ddong_kka.board_api.comment.repository.CommentRepository;
 import com.ddong_kka.board_api.exception.BoardNotFoundException;
+import com.ddong_kka.board_api.exception.CommentNotFoundException;
+import com.ddong_kka.board_api.exception.UnauthorizedAccessException;
 import com.ddong_kka.board_api.exception.UserNotFoundException;
 import com.ddong_kka.board_api.user.domain.User;
 import com.ddong_kka.board_api.user.repository.UserRepository;
@@ -23,6 +25,7 @@ public class CommentService {
     private final JwtUtil jwtUtil;
 
     public void saveComment(CommentWriteDto commentWriteDto, Long id, String jwtToken) {
+        // create에서 사용하는 id는 게시글의 id 이다.
         String userEmail = jwtUtil.getEmail(jwtToken);
 
         User user = userRepository.findByEmail(userEmail).orElseThrow(()-> new UserNotFoundException("사용자를 찾을 수 없습니다 : "+ userEmail));
@@ -36,5 +39,24 @@ public class CommentService {
                 .build();
 
         commentRepository.save(comment);
+    }
+
+    public void updateComment(CommentWriteDto commentWriteDto, Long id, String jwtToken) {
+        // update에서 사용하는 id는 댓글의 id 이다.
+
+        String userEmail = jwtUtil.getEmail(jwtToken);
+
+        User user = userRepository.findByEmail(userEmail).orElseThrow(()-> new UserNotFoundException("사용자를 찾을 수 없습니다 : "+ userEmail));
+
+        Comment targetComment = commentRepository.findById(id).orElseThrow(() -> new CommentNotFoundException("댓글을 찾을 수 없습니다 : ID = " + id));
+
+        if (!targetComment.getUser().equals(user)){
+            throw new UnauthorizedAccessException("권한이 없습니다.");
+        }
+
+
+        targetComment.setContent(commentWriteDto.getContent());
+        commentRepository.save(targetComment);
+
     }
 }
